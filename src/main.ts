fa646f0fa2e78aab327e3147b13a184357287b2b
Ron S <ron@nonara.com>
2023-06-20 22:47:23 -0400

    feat: Added nonzero exit if errors encountered

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ export function run(isDryRun: boolean = false) {
     const { tsRepoUrl } = storage.settings;
 
     /* Build */
+    let encounteredErrors = false;
     let updatedBuilds: BuildDetail[] = [];
     for (const tag of tags) {
       const context = { ...baseContext };
@@ -64,6 +65,7 @@ export function run(isDryRun: boolean = false) {
         /* Update build detail */
         buildDetail.complete = true;
       } catch (err) {
+        encounteredErrors = true;
         console.error(`Error: [${tag}] ${err}`);
         buildDetail.complete = false;
       } finally {
@@ -77,6 +79,11 @@ export function run(isDryRun: boolean = false) {
       storage.save(isDryRun);
 
       if (!isDryRun) execCmd(`git push`, { env: { ...process.env }, cwd: baseContext.repoRootDir });
+    }
+
+    if (encounteredErrors) {
+      console.error(`Finished with errors — see log above!`);
+      process.exit(1);
     }
   });
 }
